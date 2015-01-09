7f21b81178228f2734bed2dfafb5e49a9d1913af
schoeu <schoeu@163.com>
2015-01-09 17:25:59 +0800

    fixed when toast dispeared then delete itself.

--- src/sToast.js
+++ src/sToast.js
@@ -1,8 +1,15 @@
 
 /*
-*   sToast("input","信息",2000);
+*   sToast({ele:"input",msg:"信息信息",time:2000});
+*   @param msg必选，其他依需求
 * */
-function sToast(ele,msg,sec){
+function sToast(options){
+    if(options){
+        var ele = options.ele,
+            msg = options.msg || "",
+            sec = options.time || 2000;
+    }
+
     var s_body = document.querySelector("body"),timer=null,
         h = window.innerHeight,w = window.innerWidth;
 
@@ -13,11 +20,20 @@ function sToast(ele,msg,sec){
     var th = st.offsetHeight,tw = st.offsetWidth;
     st.style.left = (w-tw)/2+"px";
     st.style.top = (h-th)/2-20+"px";
-    document.querySelector(ele).addEventListener("click",function(){
+
+    //如果不是绑定元素触发
+    if(ele){
+        document.querySelector(ele).addEventListener("click",ani,false);
+    }else{
+        ani();
+    }
+
+    function ani(){
         st.style.webkitAnimation="stShows "+sec/1000+"s ease-in-out";
         if(timer) clearTimeout(timer);
         timer = setTimeout(function(){
             st.style.webkitAnimation="";
         },sec);
-    },false);
+    }
+
 }
