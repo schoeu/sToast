2d0a9a96d1c72a2a6e62af04358e7db0a0029ca9
schoeu <schoeu@163.com>
2015-02-11 20:45:20 +0800

    reduce elment bind

--- src/sToast.js
+++ src/sToast.js
@@ -5,12 +5,15 @@
 * */
 function sToast(options){
     if(options){
-        var ele = options.ele,
-            msg = options.msg || "",
+        var msg = options.msg || "",
             sec = options.time || 2000;
     }
-    var oldT = document.querySelector(".s_toast");
-    if(oldT) oldT.style.display = "none";
+    var oldT = document.querySelectorAll(".s_toast");
+    if(oldT){
+        for(var m=0;m<oldT.length;m++){
+            oldT[m].style.display = "none";
+        }
+    }
     var s_body = document.querySelector("body"),timer=null,
         h = window.innerHeight,w = window.innerWidth;
     var st = document.createElement("p");
@@ -20,21 +23,17 @@ function sToast(options){
     var th = st.offsetHeight,tw = st.offsetWidth;
     st.style.left = (w-tw)/2+"px";
     st.style.top = (h-th)/2-20+"px";
-
-    //如果不是绑定元素触发
-    if(ele){
-        document.querySelector(ele).addEventListener("click",ani,false);
-    }else{
-        ani();
-    }
-
-    function ani(){
+    (function ani(){
         st.style.webkitAnimation="stShows "+sec/1000+"s ease-in-out";
         if(timer) clearTimeout(timer);
         timer = setTimeout(function(){
             st.style.webkitAnimation="";
-            s_body.removeChild(st);
-        },sec);
-    }
+            var oT = document.querySelector(".s_toast");
+            if(oT){
+                s_body.removeChild(oT);
+            }
+
+        },sec+500);
+    })();
 
 }
